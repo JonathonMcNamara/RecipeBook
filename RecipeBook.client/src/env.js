f73779db4270320c4b5c0653e59c68e3f8334894
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-09-15 14:46:40 -0600

    env

--- RecipeBook.client/src/env.js
+++ RecipeBook.client/src/env.js
@@ -1,6 +1,6 @@
 export const dev = window.location.origin.includes('localhost')
-export const baseURL = dev ? 'http://localhost:5001' : ''
+export const baseURL = dev ? 'https://localhost:5001' : ''
 export const useSockets = false
 export const domain = 'jonmcnamara.us.auth0.com'
 export const clientId = 'yCkx457MBqe9B4kW7LJOWhFUgU4LPjIk'
-export const audience = 'https://codeworksclassroom.com'
+export const audience = 'https://jonmcnamara.us.auth0.com/api/v2/'
