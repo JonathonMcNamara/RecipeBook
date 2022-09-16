0eba963e9d91748f93d6c90251229cdec3643be5
Jonathon McNamara <jonmcnamara1234@hotmail.com>
2022-09-16 14:13:44 -0600

    models

--- RecipeBook/Models/Ingredient.cs
+++ RecipeBook/Models/Ingredient.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace RecipeBook.Models
 {
     public class Ingredient
     {
-        
+        public int Name { get; set; }
+        public int Quantity { get; set; }
+        public int RecipeId { get; set; }
     }
 }
--- RecipeBook/Models/Step.cs
+++ RecipeBook/Models/Step.cs
@@ -1,12 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace RecipeBook.Models
 {
     public class Step
     {
-        
+        public int Position { get; set; }
+        public string Body { get; set; }
+        public int RecipeId { get; set; }
     }
 }
